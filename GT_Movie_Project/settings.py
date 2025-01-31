114c7e55861631429f8f1c859f87462068c04850
athalanki3 <athalanki3@gatech.edu>
2025-01-31 01:20:08 -0500

    Chapter 5

--- GT_Movie_Project/settings.py
+++ GT_Movie_Project/settings.py
@@ -123,3 +123,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'GT_Movie_Project/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_URL = '/media/'

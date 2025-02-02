0d6aa78c79c0906e5d431e16fcee3d5e31d8c98a
ekwan99 <emilykwan999@gmail.com>
2025-02-02 17:42:02 -0500

    Chapter 9

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
 admin.site.register(Movie,MovieAdmin)
-
+admin.site.register(Review)
